b7dad01b4dc8343dca1926052fec1880c6c8a2ce
unknown <nkosimova06@gmail.com>
2020-03-25 00:12:05 +0500

    HW2

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A = ");
+            int a = int.Parse(Console.ReadLine());
+            
+            Console.Write("B = ");
+            int b = int.Parse(Console.ReadLine());
+
+            int new_a =(a == b)?0:Math.Max(a,b);
+            b = (a == b)?0:Math.Max(a,b);
+            
+            Console.WriteLine($"new A = {new_a}");
+            Console.WriteLine($"new B = {b}");
+            Console.ReadKey();
+        }
+    }
+}
